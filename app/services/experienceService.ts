a34a74d870a86bda36974e61805c94dd92a63996
Brandon Bankert <fireninja171@gmail.com>
2023-09-07 12:26:03 -0400

    Fix linting errors and update json fetch (#2)
    When building through the "build" command, it prefetches the data at build time. This causes errors since we are not running the app when building. Now we read the file directly since the components that handle this are server side.
    
    Co-authored-by: Brandon Bankert <bankert.brandon@gmail.com>

--- app/services/experienceService.ts
+++ app/services/experienceService.ts
@@ -4,35 +4,43 @@ import {
     School 
 } from "../models"
 
+import path from 'path';
+import { promises as fs } from 'fs'
+import process from 'process'
 
 export const fetchEducationData = async (): Promise<Array<School>> => {
-    const response = await fetch(`${process.env.JSON_BASE_URL}/data/education.json`, { cache: process.env.IS_LOCAL ? 'no-cache' : 'default'})
-
-    if(!response.ok){
-        throw new Error(`Error fetching education data statusCode: ${response.status}, status: ${response.statusText}`)
+    try{
+        const directory = path.join(process.cwd(),'public','data')
+        return JSON.parse(await fs.readFile(`${directory}/education.json`,'utf-8'));
     }
-
-    return await response.json()
+    catch(exception: any){
+        //actual logging would go here...
+        console.log('Exception!',exception);
+        throw new Error('Exception reading education json file')
+    }
 }
 
 
 export const fetchProjectData = async (): Promise<ProjectsResponse> => {
-    const response = await fetch(`${process.env.JSON_BASE_URL}/data/projects.json`, { cache: process.env.IS_LOCAL ? 'no-cache' : 'default'})
-
-    if(!response.ok){
-        throw new Error(`Error fetching project data statusCode: ${response.status}, status: ${response.statusText}`)
+    try{
+        const directory = path.join(process.cwd(),'public','data')
+        return JSON.parse(await fs.readFile(`${directory}/projects.json`,'utf-8'));
     }
-
-    return await response.json()
+    catch(exception){
+        //actual logging would go here...
+        console.log('Exception!',exception);
+        throw new Error('Exception reading project json file')
+    }
 }
 
-
 export const fetchJobData = async (company: string): Promise<ProfessionalExperience> => {
-    const response = await fetch(`${process.env.JSON_BASE_URL}/data/${company}.json`, { cache: process.env.IS_LOCAL ? 'no-cache' : 'default'})
-
-    if(!response.ok){
-        throw new Error(`Error fetching job data statusCode: ${response.status}, status: ${response.statusText}`)
+    try{
+        const directory = path.join(process.cwd(),'public','data')
+        return JSON.parse(await fs.readFile(`${directory}/${company}.json`,'utf-8'));
     }
-
-    return await response.json()
+    catch(exception){
+        //actual logging would go here...
+        console.log('Exception!',exception);
+        throw new Error(`Exception reading ${company} json file`)
+    }
 }
