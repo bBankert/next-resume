0a0dd2d0273ba1226a207695be69d3d2059c9420
Brandon Bankert <fireninja171@gmail.com>
2025-08-29 17:12:00 +1000

    Fix broken tests and remove unused setup file

--- app/components/__tests__/app-bar.test.tsx
+++ app/components/__tests__/app-bar.test.tsx
@@ -1,59 +1,68 @@
 import AppBar from "../app-bar"
-import { fireEvent, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest'
+import { fireEvent, RenderResult, screen } from '@testing-library/react';
+import { describe, it, expect, vi, beforeAll, beforeEach } from 'vitest'
 import { renderWithProviders } from "../../utils/test-utils";
 import React from 'react'
+import { drawerSlice } from '../../../libs'
 
 describe('appBar', () => {
+
+    let renderer: RenderResult;
+
+    //using spies as there is some jankiness, where rendering with providers multiple times
+    //causes it to duplicate the entire DOM...
+    const selectOpenSpy = vi.spyOn(drawerSlice.selectors, 'selectOpen');
+
+    beforeAll(() => {
+        renderer = renderWithProviders(<AppBar />, {
+            preloadedState: {
+                drawer: {
+                    open: false
+                }
+            }
+        })
+    })
+
+    beforeEach(() => {
+        selectOpenSpy.mockReturnValue(false);
+    })
+
     describe('when the appbar is closed', () => {
         it('should render the toggle button', () => {
-            renderWithProviders(<AppBar />,{
-                preloadedState: {
-                    drawer: {
-                        open: false
-                    }
-                }
-            })
+            renderer.rerender(<AppBar />)
 
             const toggleButton = screen.queryByTestId('navbar-toggle');
-    
+
             expect(toggleButton).not.toBeNull();
         })
 
         describe('when the open menu button is clicked', () => {
             it('should open the app bar', () => {
-                renderWithProviders(<AppBar />,{
-                    preloadedState: {
-                        drawer: {
-                            open: false
-                        }
-                    }
-                })
+                renderer.rerender(<AppBar />)
 
                 const toggleButton = screen.getByTestId('navbar-toggle');
-    
+
                 fireEvent.click(toggleButton)
-    
+
                 const navLinks = screen.queryByTestId('nav-links');
                 expect(navLinks?.classList).not.toContain('hidden')
-                
+
             })
         })
     })
 
     describe('when the appbar is open', () => {
+
+        beforeEach(() => {
+            selectOpenSpy.mockReturnValue(true);
+        })
+
         describe('when the overlay is clicked', () => {
             it('should close the appbar', () => {
-                renderWithProviders(<AppBar />,{
-                    preloadedState: {
-                        drawer: {
-                            open: true
-                        }
-                    }
-                })
+                renderer.rerender(<AppBar />)
 
                 const navbarOverlay = screen.getByTestId('navbar-overlay');
-                
+
 
                 fireEvent.click(navbarOverlay);
 
@@ -65,36 +74,23 @@ describe('appBar', () => {
 
         describe('when a link is clicked', () => {
             it('should close the appbar', () => {
-                renderWithProviders(<AppBar />,{
-                    preloadedState: {
-                        drawer: {
-                            open: true
-                        }
-                    }
-                })
+                renderer.rerender(<AppBar />)
 
                 const navLink = screen.getByTestId('nav-link');
-                
+
                 fireEvent.click(navLink);
 
-                const navLinks = screen.queryByTestId('nav-links');
+                const hiddenLinkSection = screen.getByTestId('nav-links')
 
-                expect(navLinks?.classList).toContain('hidden')
+                expect(hiddenLinkSection?.classList).toContain('hidden')
             })
         })
 
         describe('when the close button is clicked', () => {
             it('should close the appbar', () => {
-                renderWithProviders(<AppBar />,{
-                    preloadedState: {
-                        drawer: {
-                            open: true
-                        }
-                    }
-                })
+                renderer.rerender(<AppBar />)
 
                 const closeButton = screen.getByTestId('navbar-close-button');
-                
 
                 fireEvent.click(closeButton);
 
